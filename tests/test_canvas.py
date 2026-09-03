8c1d9658909f6fa94766f2f6eb94698019ada404
Angus Hollands <goosey15@gmail.com>
2026-09-03 17:00:08 +0100

    feat: improve tests

--- tests/test_canvas.py
+++ tests/test_canvas.py
@@ -9,13 +9,35 @@ from jupyterhub_oauthenticator_authz_helpers.canvas import (
 
 
 @pytest.mark.asyncio
-async def test_canvas_courses(mock_response):
+async def test_canvas_courses_default(mock_response):
     with mock_response("canvas", get_courses):
-        groups = await get_course_groups("", "", "course_code")
+        groups = await get_course_groups("", "")
+
+    assert groups == [
+        "course::2i2c&20JupyterHub&20Integration&20Testing",
+        "course::2i2c&20JupyterHub&20Integration&20Testing::enrollment_type::teacher",
+    ]
+
+
+@pytest.mark.asyncio
+async def test_canvas_courses_code(mock_response):
+    with mock_response("canvas", get_courses):
+        groups = await get_course_groups("", "", canvas_course_key="course_code")
 
     assert groups == [
         "course::2i2c-jupyter",
         "course::2i2c-jupyter::enrollment_type::teacher",
+    ]
+
+
+@pytest.mark.asyncio
+async def test_canvas_courses_id(mock_response):
+    with mock_response("canvas", get_courses):
+        groups = await get_course_groups("", "", canvas_course_key="id")
+
+    assert groups == [
+        "course::3248",
+        "course::3248::enrollment_type::teacher",
     ]
 
 
@@ -25,3 +47,30 @@ async def test_canvas_users(mock_response):
         groups = await get_user_groups("", "")
 
     assert groups == ["course::3::group::Math&20Group&201"]
+
+
+# Now test setions
+@pytest.mark.asyncio
+async def test_canvas_sections_default(mock_response):
+    with mock_response("canvas_sections", get_courses):
+        groups = await get_course_groups("", "")
+
+    assert groups == [
+        # Check that the default groups are there
+        "course::2i2c&20JupyterHub&20Integration&20Testing",
+        "course::2i2c&20JupyterHub&20Integration&20Testing::enrollment_type::teacher",
+        # Test the name of section
+        "course::2i2c&20JupyterHub&20Integration&20Testing::section::2i2c&20Jupyter&20Test&20Course",
+    ]
+
+
+@pytest.mark.asyncio
+async def test_canvas_sections_id(mock_response):
+    with mock_response("canvas_sections", get_courses):
+        groups = await get_course_groups("", "", canvas_section_key="id")
+
+    assert groups == [
+        "course::2i2c&20JupyterHub&20Integration&20Testing",
+        "course::2i2c&20JupyterHub&20Integration&20Testing::enrollment_type::teacher",
+        "course::2i2c&20JupyterHub&20Integration&20Testing::section::476176",
+    ]
--- tests/test_mastodon.py
+++ tests/test_mastodon.py
@@ -7,12 +7,24 @@ from jupyterhub_oauthenticator_authz_helpers.mastodon import (
 
 
 @pytest.mark.asyncio
-async def test_canvas_courses(mock_response):
+async def test_mastodon_courses_remap(mock_response):
     id_to_alias = {"1": "some-user", "2": "another-user"}
 
     with mock_response("mastodon", get_relationships):
-        groups = await get_followed_groups("", "", id_to_alias)
+        groups = await get_followed_groups("", "", allow_list=id_to_alias)
 
     assert groups == [
         "following::some-user",
+    ]
+
+
+@pytest.mark.asyncio
+async def test_mastodon_courses(mock_response):
+    allow_list = ["1", "2"]
+
+    with mock_response("mastodon", get_relationships):
+        groups = await get_followed_groups("", "", allow_list=allow_list)
+
+    assert groups == [
+        "following::1",
     ]
